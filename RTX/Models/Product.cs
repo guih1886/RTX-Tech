7aa119e9571db04f2325ed07b742fa92046f5632
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-21 12:23:26 -0300

    Adicionado validações para criar produto
    Adicionado validações para criar produto

--- RTX/Models/Product.cs
+++ RTX/Models/Product.cs
@@ -17,8 +17,12 @@ public class Product
 
     [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Digite a url da imagem.")]
+    [Url(ErrorMessage = "A url da imagem é inválida.")]
     public string Photo { get; set; }
+    [Required(ErrorMessage = "Digite a nome do produto.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Digite a descrição do produto.")]
     public string Description { get; set; }
     public double Price { get; set; }
 }
